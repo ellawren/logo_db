f533e161a1716c6e4a3d6110425bec88c886edc5
Erin Lawrence <el2891@gmail.com>
2012-10-02 14:14:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LogoDb::Application.config.secret_token = 'e215bf60f4f9bd225cf11dd26db504aa948c6710b912766f1f5a22a2844cfac647ba66daf9e9b89914bff3e8e8178a410d05cad37fd51593ee991cc8a023bcc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LogoDb::Application.config.session_store :cookie_store, key: '_logo_db_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LogoDb::Application.config.session_store :active_record_store
